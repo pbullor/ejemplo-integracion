cbe8ec05a6157ba24fde208993a4a1a5faf3d4f1
pbullor <pablobullor@gmail.com>
2021-10-26 00:15:31 +0000

    subo ejemplo

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -24,6 +24,49 @@ const getState = ({ getStore, getActions, setStore }) => {
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
+			login: (user, password) => {
+				console.log("usuario", user);
+				console.log("password", password);
+				var myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				var raw = JSON.stringify({
+					user_name: user,
+					password: password
+				});
+
+				var requestOptions = {
+					method: "POST",
+					headers: myHeaders,
+					body: raw,
+					redirect: "follow"
+				};
+
+				fetch("https://3000-lavender-skunk-e2efosga.ws-us17.gitpod.io/login", requestOptions)
+					.then(response => {
+						console.log("status", response.status);
+						return response.json();
+					})
+					.then(result => {
+						localStorage.setItem("token", result.data.token);
+					})
+					.catch(error => console.log("error", error));
+			},
+			getPlanets: () => {
+				var myHeaders = new Headers();
+				myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
+
+				var requestOptions = {
+					method: "GET",
+					headers: myHeaders,
+					redirect: "follow"
+				};
+
+				fetch("https://3000-lavender-skunk-e2efosga.ws-us17.gitpod.io/planets", requestOptions)
+					.then(response => response.text())
+					.then(result => console.log(result))
+					.catch(error => console.log("error", error));
+			},
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,67 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useState } from "react";
+import { Redirect } from "react-router-dom";
+// import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [user, setUser] = useState();
+	const [password, setPassword] = useState();
+	const [auth, setAuth] = useState(false);
+
+	const onSubmit = e => {
+		e.preventDefault();
+		actions.login(user, password);
+		actions.getPlanets();
+		if (localStorage.getItem("token") != "") setAuth(true);
+	};
+
+	return (
+		<div className="text-center mt-5">
+			{auth ? (
+				<Redirect to="/demo" />
+			) : (
+				<form onSubmit={onSubmit}>
+					<div className="mb-3">
+						<label htmlFor="exampleInputEmail1" className="form-label">
+							user
+						</label>
+						<input
+							type="text"
+							className="form-control"
+							id="exampleInputEmail1"
+							aria-describedby="emailHelp"
+							onChange={e => setUser(e.target.value)}
+							value={user}
+						/>
+						<div id="emailHelp" className="form-text">
+							Well never share your email with anyone else.
+						</div>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="exampleInputPassword1" className="form-label">
+							Password
+						</label>
+						<input
+							type="password"
+							className="form-control"
+							id="exampleInputPassword1"
+							onChange={e => setPassword(e.target.value)}
+							value={password}
+						/>
+					</div>
+					<div className="mb-3 form-check">
+						<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+						<label className="form-check-label" htmlFor="exampleCheck1">
+							Check me out
+						</label>
+					</div>
+					<button type="submit" className="btn btn-primary">
+						Submit
+					</button>
+				</form>
+			)}
+		</div>
+	);
+};
